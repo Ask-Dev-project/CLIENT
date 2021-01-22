e8497c8b8b4d3e8c913ae297dcbce9858a0756d0
alfatio <alfatioutama@gmail.com>
2021-01-22 14:13:56 +0700

    init

---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import Axios from 'axios';
+import React from 'react';
+
+function Home(props) {
+
+    return (
+        <>
+            <button>join</button>
+        </>
+    )
+}
+
+export default Home;
